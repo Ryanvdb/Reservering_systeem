74899de6a354b791f383d6aa1a7e9173efaadf42
Ryan van den berg <ryan.berg35@outlook.com>
2018-09-19 12:31:27 +0200

    FIXED IT

--- Reservering_Systeem/Reservering_Systeem/Form1.Designer.cs
+++ Reservering_Systeem/Reservering_Systeem/Form1.Designer.cs
@@ -28,9 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             this.SuspendLayout();
             // 
             // Form1
@@ -38,30 +35,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(651, 389);
-=======
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 73);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.button1);
->>>>>>> 6ce0a89a9681217014253d8f7a7722c4fb211034
-=======
-            this.button1 = new System.Windows.Forms.Button();
-=======
->>>>>>> 9a7a68189ff12e5c82cef6967cb58e2a2875a65c
             this.SuspendLayout();
             // 
             // Form1
@@ -69,17 +52,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
-<<<<<<< HEAD
-            this.Controls.Add(this.button1);
->>>>>>> 6ce0a89a9681217014253d8f7a7722c4fb211034
-=======
->>>>>>> 9a7a68189ff12e5c82cef6967cb58e2a2875a65c
+
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
-
+            
         #endregion
     }
 }
