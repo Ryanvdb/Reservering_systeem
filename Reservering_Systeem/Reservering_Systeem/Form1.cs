ca111eaa87226b0d6cefb0c80967c306d2646482
Ricardo Buitenhuis <meowoo@live.nl>
2018-09-19 12:31:34 +0200

    My version

--- Reservering_Systeem/Reservering_Systeem/Form1.cs
+++ Reservering_Systeem/Reservering_Systeem/Form1.cs
@@ -16,5 +16,10 @@ namespace Reservering_Systeem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Show();
+        }
     }
 }
