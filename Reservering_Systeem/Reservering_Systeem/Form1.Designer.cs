94c29db0dfbe94a054fb55a8b1489cc06bb00fe6
Ryan van den berg <ryan.berg35@outlook.com>
2018-09-20 13:11:36 +0200

    listview is GOOOONE

--- Reservering_Systeem/Reservering_Systeem/Form1.Designer.cs
+++ Reservering_Systeem/Reservering_Systeem/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView = new System.Windows.Forms.ListView();
             this.imagePanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -40,21 +39,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // listView
-            // 
-            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView.Location = new System.Drawing.Point(0, 0);
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(1902, 1033);
-            this.listView.TabIndex = 0;
-            this.listView.UseCompatibleStateImageBehavior = false;
-            // 
             // imagePanel
             // 
             this.imagePanel.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.imagePanel.Location = new System.Drawing.Point(1564, 104);
+            this.imagePanel.Location = new System.Drawing.Point(1173, 84);
+            this.imagePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.imagePanel.Name = "imagePanel";
-            this.imagePanel.Size = new System.Drawing.Size(326, 760);
+            this.imagePanel.Size = new System.Drawing.Size(244, 618);
             this.imagePanel.TabIndex = 1;
             // 
             // pictureBox1
@@ -62,49 +53,55 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(264, 104);
+            this.pictureBox1.Location = new System.Drawing.Point(198, 84);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1294, 760);
+            this.pictureBox1.Size = new System.Drawing.Size(970, 618);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // panel1
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.panel1.Location = new System.Drawing.Point(264, 870);
+            this.panel1.Location = new System.Drawing.Point(198, 707);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1294, 151);
+            this.panel1.Size = new System.Drawing.Size(970, 123);
             this.panel1.TabIndex = 2;
             // 
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.panel2.Location = new System.Drawing.Point(12, 104);
+            this.panel2.Location = new System.Drawing.Point(9, 84);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(246, 917);
+            this.panel2.Size = new System.Drawing.Size(184, 745);
             this.panel2.TabIndex = 2;
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Location = new System.Drawing.Point(9, 10);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1546, 86);
+            this.panel3.Size = new System.Drawing.Size(1160, 70);
             this.panel3.TabIndex = 3;
             // 
             // panel4
             // 
             this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel4.Location = new System.Drawing.Point(1564, 12);
+            this.panel4.Location = new System.Drawing.Point(1173, 10);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(326, 86);
+            this.panel4.Size = new System.Drawing.Size(244, 70);
             this.panel4.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(1564, 967);
+            this.button1.Location = new System.Drawing.Point(1173, 786);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 54);
+            this.button1.Size = new System.Drawing.Size(200, 44);
             this.button1.TabIndex = 5;
             this.button1.Text = "Reserveren";
             this.button1.UseVisualStyleBackColor = true;
@@ -112,18 +109,19 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(1836, 967);
+            this.button2.Location = new System.Drawing.Point(1377, 786);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 54);
+            this.button2.Size = new System.Drawing.Size(42, 44);
             this.button2.TabIndex = 5;
             this.button2.Text = "<";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.ClientSize = new System.Drawing.Size(1426, 839);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel4);
@@ -132,8 +130,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.imagePanel);
-            this.Controls.Add(this.listView);
-            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -142,8 +139,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.Panel imagePanel;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel1;
