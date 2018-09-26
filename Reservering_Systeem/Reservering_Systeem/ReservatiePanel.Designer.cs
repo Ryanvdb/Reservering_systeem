1af11a611c9cd059023d789b9c9f1fd929b0919f
Ricardo Buitenhuis <meowoo@live.nl>
2018-09-26 12:16:08 +0200

    panic push

--- Reservering_Systeem/Reservering_Systeem/ReservatiePanel.Designer.cs
+++ Reservering_Systeem/Reservering_Systeem/ReservatiePanel.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "ReservatiePanel";
-            this.Size = new System.Drawing.Size(382, 53);
+            this.Size = new System.Drawing.Size(392, 53);
             this.ResumeLayout(false);
 
         }
